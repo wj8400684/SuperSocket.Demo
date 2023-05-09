bccf852c9e1716fa34fa36b0ddf22f089bb9be3a
wujun <8400684@qq.com>
2023-05-09 21:32:21 +0800

    add send package

--- Google.Protobuf.Server/Server/CommandSession.cs
+++ Google.Protobuf.Server/Server/CommandSession.cs
@@ -1,12 +1,20 @@
 using Core;
+using SuperSocket.ProtoBase;
 using SuperSocket.Server;
 
 namespace Server;
 
 public sealed class CommandSession : AppSession
 {
+    private readonly IPackageEncoder<CommandPackage> _encoder;
+
+    public CommandSession(IServiceProvider serviceProvider)
+    {
+        _encoder = serviceProvider.GetRequiredService<IPackageEncoder<CommandPackage>>();
+    }
+    
     internal ValueTask SendPackageAsync(CommandPackage package)
     {
-        
+        return Channel.IsClosed ? ValueTask.CompletedTask : Channel.SendAsync(_encoder, package);
     }
 }
